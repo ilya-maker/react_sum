a129becd45966e4c61b3a5821d365c6c29cce1a8
Illia Oliynik <illiaoliynik@gmail.com>
2020-04-23 12:35:00 +0300

    Updates

---
+++ precache-manifest.909cfb4da31c6fef48476b9ca9071851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef3f4de883d0da99bf25847d31d1066",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f28560cd54ef99d9bd1",
+    "url": "./static/js/2.e52bef64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e52bef64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2afaf41a657cadba65ee",
+    "url": "./static/js/main.60cab661.chunk.js"
+  },
+  {
+    "revision": "8f752b305088beda2000",
+    "url": "./static/js/runtime-main.9f2fd0c6.js"
+  }
+]);
